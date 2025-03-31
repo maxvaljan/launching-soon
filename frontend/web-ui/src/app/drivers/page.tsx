b5599b4723aab0bd16ac3134af6740b137b476c5
Anshul <schul.github@gmail.com>
2025-03-31 22:47:45 +0800

    Updated page.tsx

--- frontend/web-ui/src/app/drivers/page.tsx
+++ frontend/web-ui/src/app/drivers/page.tsx
@@ -137,7 +137,7 @@ export default function Drivers() {
         </div>
 
         {/* Requirements Section */}
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-16 mb-8">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-16 mb-16">
             <Card>
               <CardContent className="pt-6">
                 <Shield className="w-12 h-12 text-maxmove-800 mb-4" />
