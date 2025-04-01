7d12fd143916de9f858ab6b64cd97f2e66360b6f
Max <max.valjan@gmail.com>
2025-04-01 15:28:20 +0200

    refactor: simplify investment page to only include Hero and BackedByScience sections

--- frontend/web-ui/src/app/investment/page.tsx
+++ frontend/web-ui/src/app/investment/page.tsx
@@ -1,21 +1,16 @@
 'use client';
 
 import { useEffect } from 'react';
-import Hero from "@/components/investment/Hero";
-import BackedByScience from "@/components/investment/BackedByScience";
-import Info from "@/components/investment/Info";
-import Future from "@/components/investment/Future";
-import CTASection from "@/components/investment/CTASection";
-import FundingSection from "@/components/investment/FundingSection";
-import HyperlogisticsSection from "@/components/investment/HyperlogisticsSection";
-import { checkAdminStatus } from "@/lib/services/reports";
+import Hero from '@/components/investment/Hero';
+import BackedByScience from '@/components/investment/BackedByScience';
+import { checkAdminStatus } from '@/lib/services/reports';
 
 export default function InvestmentPage() {
   useEffect(() => {
     const checkAdmin = async () => {
       await checkAdminStatus();
     };
-    
+
     checkAdmin();
   }, []);
 
@@ -23,11 +18,6 @@ export default function InvestmentPage() {
     <div className="min-h-screen bg-[#0d0f1a]">
       <Hero />
       <BackedByScience />
-      <Info />
-      <Future />
-      <FundingSection />
-      <HyperlogisticsSection />
-      <CTASection />
     </div>
   );
 }
