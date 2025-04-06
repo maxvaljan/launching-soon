58032750ec4924ce12b508b42a68f0b83ef3c833
Max <max.valjan@gmail.com>
2025-04-06 11:43:26 +0200

    fix(customer-ui): explicitly set source/asset extensions in metro config

--- frontend/customer-ui/metro.config.js
+++ frontend/customer-ui/metro.config.js
@@ -9,6 +9,10 @@ const monorepoRoot = path.resolve(projectRoot, '../..');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(projectRoot);
 
+// Get the default source extensions
+const defaultSourceExts = require('metro-config/src/defaults/defaults').sourceExts;
+const defaultAssetExts = require('metro-config/src/defaults/defaults').assetExts;
+
 // 1. Watch all files within the monorepo
 config.watchFolders = [monorepoRoot];
 
@@ -21,7 +25,16 @@ config.resolver.nodeModulesPaths = [
 // 3. Force Metro to resolve (sub)dependencies only from the monorepo's node_modules
 config.resolver.disableHierarchicalLookup = true;
 
-// 4. Support shared packages
+// 4. Explicitly set source and asset extensions
+config.resolver.sourceExts = process.env.RN_SRC_EXT
+  ? [...process.env.RN_SRC_EXT.split(','), ...defaultSourceExts]
+  : defaultSourceExts;
+if (config.resolver.assetExts) {
+  config.resolver.assetExts = config.resolver.assetExts.filter((ext) => ext !== "svg");
+}
+config.resolver.assetExts = [...defaultAssetExts, "svg"]; // Ensure svg is treated as an asset
+
+// 5. Support shared packages
 config.resolver.extraNodeModules = {
   shared: path.resolve(monorepoRoot, 'shared'),
 };
